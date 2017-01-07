431aa819f90c0d18302e7d63e42cd291e9771862
Wojo <wojo1330@gmail.com>
2017-01-06 20:04:27 -0700

    initial commit

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '684650761714100',
+    secret: '5bcf83be6b39d4555f99fa193478a5d3'
+});
